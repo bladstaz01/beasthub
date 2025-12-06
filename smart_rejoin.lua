ec51bdb323b5343f5462c440248876ee8ba891b4
bladstaz01 <bladstaz01@gmail.com>
2025-12-07 04:52:54 +0800

    Update smart_rejoin.lua

--- smart_rejoin.lua
+++ smart_rejoin.lua
@@ -1,1 +1,6 @@
+local M = {}
+function M.init(Rayfield, beastHubNotify, Window, myFunctions, beastHubIcon, equipItemByName, equipItemByNameV2, getMyFarm, getFarmSpawnCFrame, getAllPetNames, sendDiscordWebhook)
+  
+end
 
+return M
