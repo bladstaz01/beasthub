dd5c3f9e84c45c0781dc4a4b3458df25aa643388
bladstaz01 <bladstaz01@gmail.com>
2025-12-07 05:06:22 +0800

    Delete smart_rejoin.lua

--- smart_rejoin.lua
+++
@@ -1,6 +0,0 @@
-local M = {}
-function M.init(Rayfield, beastHubNotify, Window, myFunctions, beastHubIcon, equipItemByName, equipItemByNameV2, getMyFarm, getFarmSpawnCFrame, getAllPetNames, sendDiscordWebhook)
-  
-end
-
-return M
