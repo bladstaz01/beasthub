7752b1ba4809f4b5038943239b5844ffa1f53a08
bladstaz01 <bladstaz01@gmail.com>
2025-12-19 06:25:25 +0800

    Update trader.lua

--- trader.lua
+++ trader.lua
@@ -1,1 +1,12 @@
 
+local M = {}
+
+
+function M.init(Rayfield, beastHubNotify, Window, myFunctions, beastHubIcon, equipItemByName, equipItemByNameV2, getMyFarm, getFarmSpawnCFrame, getAllPetNames, sendDiscordWebhook)
+    local Trader = Window:CreateTab("Trader", "handshake")
+
+
+end
+
+
+return M
